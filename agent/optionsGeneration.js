2d5953668a35fb42f7aee578661f3e49174127c2
Ella Bergström <bergstrom.ella1@gmail.com>
2026-05-06 14:13:29 +0200

    Working pick up and deliver for one package!

--- agent/optionsGeneration.js
+++ agent/optionsGeneration.js
@@ -2,35 +2,37 @@ import { parcels } from "../beliefs/parcels.js";
 import { me } from "../beliefs/me.js";
 import { distance } from "../utils/distance.js";
 
- /**
- * Options generation and filtering function
- */
-export function optionsGeneration (myAgent) {
+export function optionsGeneration(myAgent) {
 
-    // TODO revisit beliefset revision so to trigger option generation only in the case a new parcel is observed
+    /**
+     * If carrying, only deliver - don't pick up more
+     */
+    const carrying = Array.from(parcels.values()).filter(p => p.carriedBy === me.id);
+    if (carrying.length > 0) {
+        myAgent.push(['go_deliver']);
+        return;
+    }
 
     /**
      * Options generation
-     * @type { Array< [string, ...any] > }
      */
-    const options = []
+    const options = [];
     for (const parcel of parcels.values())
-        if ( ! parcel.carriedBy )
-            options.push( [ 'go_pick_up', parcel.x, parcel.y, parcel.id ] );
-            // myAgent.push( [ 'go_pick_up', parcel.x, parcel.y, parcel.id ] )
+        if (!parcel.carriedBy)
+            options.push(['go_pick_up', parcel.x, parcel.y, parcel.id]);
 
     /**
-     * Options filtering
+     * Options filtering - find nearest parcel
      */
     let best_option;
     let nearest = Number.MAX_VALUE;
     for (const option of options) {
-        if ( option[0] == 'go_pick_up' ) {
-            let [go_pick_up,x,y,id] = option;
-            let current_d = distance( {x, y}, me )
-            if ( current_d < nearest ) {
-                best_option = option
-                nearest = current_d
+        if (option[0] == 'go_pick_up') {
+            let [go_pick_up, x, y, id] = option;
+            let current_d = distance({ x, y }, me);
+            if (current_d < nearest) {
+                best_option = option;
+                nearest = current_d;
             }
         }
     }
@@ -38,15 +40,6 @@ export function optionsGeneration (myAgent) {
     /**
      * Best option is selected
      */
-    if ( best_option )
-        myAgent.push( best_option )
-
-    /**
-     * Delivery option
-     * If agent is carrying any parcel, push go_deliver
-     */
-    const carrying = Array.from(parcels.values()).filter(p => p.carriedBy === me.id);
-    if (carrying.length > 0)
-        myAgent.push(['go_deliver']);
-
+    if (best_option)
+        myAgent.push(best_option);
 }
--- beliefs/deliveryTiles.js
+++ beliefs/deliveryTiles.js
@@ -5,7 +5,7 @@ export function initDeliveryTiles(socket) {
     socket.onMap((width, height, tiles) => {
         console.log("TILES:", tiles);
         for (const tile of tiles) {
-            if (tile.type === 3) {
+            if (tile.type === 2) {
                 deliveryTiles.push(tile);
             }
         }
