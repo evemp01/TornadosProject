46eae580fe1d7d5b269cccd70cfd1a7759fdd052
Ella Bergström <bergstrom.ella1@gmail.com>
2026-05-07 12:49:21 +0200

    Types of tiles check works better

--- beliefs/map.js
+++ beliefs/map.js
@@ -2,23 +2,26 @@ export const grid = new Map();
 export const deliveryTiles = [];
 export const spawnTiles = [];
 export const blackTiles = [];
+export const whiteTiles = [];
 export let mapWidth = 0;
 export let mapHeight = 0;
 
 export function initMap(socket) {
+    console.log('initMap called, registering onMap listener');
     socket.onMap((width, height, tiles) => {
         mapWidth = width;
         mapHeight = height;
+        console.log('onMap fired! tiles:', tiles.length);
 
         for (const tile of tiles) {
             grid.set(`${tile.x},${tile.y}`, tile.type);
-
-            if (tile.type === 0) blackTiles.push(tile);
-            if (tile.type === 1) spawnTiles.push(tile);
-            if (tile.type === 2) deliveryTiles.push(tile);
+            console.log(tile.type);
+            if (tile.type == 0) blackTiles.push(tile);
+            if (tile.type == 1) spawnTiles.push(tile);
+            if (tile.type == 2) deliveryTiles.push(tile);
+            if (tile.type == 3) whiteTiles.push(tile);
         }
-
-        console.log(`Map loaded: ${width}x${height}, ${tiles.length} tiles, ${deliveryTiles.length} delivery tiles`);
+        console.log('Map initialized. Delivery tiles:', deliveryTiles.length, 'Spawn tiles:', spawnTiles.length, 'White tiles:', whiteTiles.length);
     });
 }
 
@@ -26,17 +29,6 @@ export function isWalkable(x, y) {
     const type = grid.get(`${x},${y}`);
     return type === 1 || type === 2 || type === 3;
 }
-
-// export function initDeliveryTiles(socket) {
-//     socket.onMap((width, height, tiles) => {
-//         console.log("TILES:", tiles);
-//         for (const tile of tiles) {
-//             if (tile.type === 2) {
-//                 deliveryTiles.push(tile);
-//             }
-//         }
-//     });
-// }
 
 export function isDelivery(x, y) {
     return grid.get(`${x},${y}`) === 2;
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { DjsConnect } from "@unitn-asa/deliveroo-js-sdk/client";
 import { me, initMe } from "./beliefs/me.js";
 import { parcels, initParcels } from "./beliefs/parcels.js";
-import { initMap } from './beliefs/map.js';
+import { initMap, deliveryTiles, spawnTiles, whiteTiles } from './beliefs/map.js';
 import { IntentionRevisionRevise } from "./intentions/IntentionRevisionRevise.js";
 import { planLibrary } from "./plans/index.js";
 import { optionsGeneration } from "./agent/optionsGeneration.js";
@@ -34,6 +34,3 @@ setInterval(() => {
         Array.from(parcels.values()).map(p => `${p.id}@(${p.x},${p.y})`)
     );
 }, 1000);
-
-console.log("ME:", me);
-console.log("PARCELS:", parcels.size);
